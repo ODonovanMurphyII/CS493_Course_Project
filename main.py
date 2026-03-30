df583a8d64085bd26018d4ed2b77ea26a1271f48
Don Murphy <dm4224@gmail.com>
2026-03-30 19:14:09 -0400

    reworking the parser to be more "friendly" | Migrating over to robotparser

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import requests
+from urllib.robotparser import RobotFileParser
 
-print("Please enter URL:", end="")
+headerInfo = {
+    'User Agent': 'StudentCrawlerv1.0'
+}
+print("https://", end="")
 URL = input()
-print("Please enter a keyword to look for")
-keyword = input()
 pagesToExplore = []
 pagesToAvoid = []
 
 # First stage
-attempt = requests.get(URL)
+attempt = requests.get("https://" + URL + "/robots.txt")
 if attempt.status_code == 200:
     rawData = attempt.content
     rawData = rawData.decode('utf-8')
     strings = rawData.splitlines()
     i = 0
-    while i < len(strings):  #TODO need to make this section more robust
-        if "sitemap" in strings[i]:
-            strings[i] = strings[i].lstrip("Sitemap: ")
-            pagesToExplore.append(strings[i])
-        elif "Disallow" in strings[i]:
-            pagesToAvoid.append(strings[i])
+    currentString = rawData[i]
+    while(currentString != "User-agent: *"):
         i += 1
+        currentString = strings[i]
+    print(currentString)
+
 else:
-    print("Error:" + attempt.status_code)
+    print("Error:" + str(attempt.status_code))
 
 #eventual recursive call and possible DFS algorithm
 #for now, just getting header and URL information
