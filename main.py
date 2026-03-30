7c37cfacb94d3c7fa86580bfe0c2d4ff214a9e64
Don Murphy <dm4224@gmail.com>
2026-03-30 06:04:36 -0400

    fixed a bug where splitlnes returned byte arrays instead of string objects

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import requests
 
 print("Please enter URL:", end="")
 URL = input()
-robotsLocaiton = URL + "/robots.txt"
 pagesToExplore = []
 pagesToAvoid = []
 
-attempt = requests.get("http://www.cnn.com/robots.txt")
+attempt = requests.get(URL)
 if attempt.status_code == 200:
     rawData = attempt.content
-strings = rawData.splitlines()
-while i < len(strings):
-    if "sitemap" in strings[i]:
-        pagesToExplore.append(strings[i])
-    elif "Disallow" in strings[i]:
-        pagesToAvoid.append(strings[i])
-    i += 1
+    rawData = rawData.decode('utf-8')
+    strings = rawData.splitlines()
+    i = 0
+    while i < len(strings):
+        if "sitemap" in strings[i]:
+            pagesToExplore.append(strings[i])
+        elif "Disallow" in strings[i]:
+            pagesToAvoid.append(strings[i])
+        i += 1
+else:
+    print("Error:" + attempt.status_code)
 
 
