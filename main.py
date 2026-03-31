d980ec6465879bcf78d33c415f93f935638d7710
Don Murphy <dm4224@gmail.com>
2026-03-31 19:50:19 -0400

    Have a userAgent defined allowed list from a properly parsed robots.txt file

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import requests
 from urllib.robotparser import RobotFileParser
 
-headerInfo = {
-    'User Agent': 'StudentCrawlerv1.0@CCSU.EDU'
-}
 
-robot = RobotFileParser()
-print("Enter url")
-URL = input()
-req = requests.get(URL)
-robot.parse(req.text.splitlines())
-sitemaps = robot.site_maps()
-for sitemap in sitemaps:
-    print(sitemap)
+class Crawler:
+    def __init__(self, userAgent):
+        self.userAgent = userAgent
+        self.RobotsParser = RobotFileParser()
+        self.req = None
+        self.mSdelay = 1000 
+        pass
+
+    def parse_robots_txt(self):
+        print("Enter URL:", end="")
+        URL = input()                       ## TODO maybe its useful to hang onto the URL
+        print("Parsing File...")
+        self.req = requests.get("https://www.cnn.com/robots.txt")           ## TODO hardcoded for now
+        if (self.req.ok):
+            self.RobotsParser.parse(self.req.text.splitlines())
+        else:
+            print("Failed to parse robots.txt" + str(self.req))
+
+
+newsBot = Crawler('StudentCrawlerv1.0@CCSU.EDU')
+newsBot.parse_robots_txt()
+
+## Lets see all the sites we are allowed to crawl
+siteMaps = newsBot.RobotsParser.site_maps()
+for site in siteMaps:
+    if newsBot.RobotsParser.can_fetch(newsBot.userAgent[1],site):
+        print("We can crawl:" + site)
+    else:
+        print("Site not allowed:" + site)
+
 
 
     
