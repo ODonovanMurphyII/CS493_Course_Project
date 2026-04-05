f0154673dca45ad94e9b34f89e08c015ef1c74c0
Don Murphy <dm4224@gmail.com>
2026-04-05 11:46:15 -0400

    Adding some error checking and file handling

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import requests
+import pathlib 
+import datetime
+import os
 from urllib.robotparser import RobotFileParser
 
-
+class Logger:
+    def __init__(self):
+        self.timestamp = datetime.now()
+        self.urlsCrawled = []
+        self.noLog = False
+        try:
+            self.logFile = open("log.txt", "w", encoding="utf-8")
+        except Exception as e:
+            print(f"Error creating log file: {e}")
+            self.noLog = True
+        
 class Crawler:
     def __init__(self, userAgent):
         self.userAgent = userAgent
         self.RobotsParser = RobotFileParser()
         self.req = None
-        self.mSdelay = 1000 
-        pass
+        self.halt = 5               # delay in seconds between requests
+        self.outputFile = None
+        self.outputFilename = None
+        self.fileOutputAllowed = True
+        try:
+            self.outputDirectory = pathlib.Path(__file__).parent / "storedPages"
+            self.outputDirectory.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            self.fileOutputAllowed = False
+            print(f"Error File Output Not Allowed. Check Permissions. {e}")
 
     def parse_robots_txt(self):
         print("Enter URL:", end="")
@@ -19,6 +40,25 @@ class Crawler:
             self.RobotsParser.parse(self.req.text.splitlines())
         else:
             print("Failed to parse robots.txt" + str(self.req))
+
+    def storePages(self, sentinel):
+        currentNumberOfFiles = len(file for file in self.outputDirectory.iterdir() if file.is_file())
+        currentNumberOfFiles += 1
+        self.outputFilename = "site" + str(currentNumberOfFiles)
+        if not sentinel: 
+            print("No sentienl provided. Storing every allowable page to ~/storedPages")
+        else:
+            for site in self.RobotsParser.site_maps():
+                self.reg = requests.get(site)
+                if(self.req.ok):
+                    pass
+
+                
+            
+        
+        
+    
+    
 
 
 newsBot = Crawler('StudentCrawlerv1.0@CCSU.EDU')
@@ -31,6 +71,8 @@ for site in siteMaps:
         print("We can crawl:" + site)
     else:
         print("Site not allowed:" + site)
+
+
 
 
 
